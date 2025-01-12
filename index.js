5e628f725b046e3501674a2baff790806cbd86a8
Ayush Joshi <ayushjoshi3699@gmail.com>
2025-01-12 09:12:04 +0530

    Complet functionality

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express")
 const app = express();
 const mongoose =require("mongoose")
 const Listing=require("./models/listing.js")
+const methodOverride=require("method-override")
+const ejsMate=require("ejs-mate")
 const path=require("path");
 
 const MONGO_URI = 'mongodb://127.0.0.1:27017/airbnb';
 app.set("view engine","ejs");
+app.engine("ejs",ejsMate);
 app.set("views",path.join(__dirname,"views"));
-app.set(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(methodOverride("_method"))
 
 main()
     .then(() => {
@@ -20,6 +25,9 @@ async function main() {
 app.get("/",(req,res)=>{
     res.send("this is the base route for a website");
 })
+
+// TRAIN OUR DATABSE FROM SINGLE DATA.
+
 // app.get("/listingtest",(req,res)=>{
 //     const list1=new Listing({
 //         title:"my new villa",
@@ -50,6 +58,36 @@ app.get("/listings/:id",async (req,res)=>{
     const specificPost=await Listing.findById(id);
     res.render("./listings/show.ejs",{specificPost});
 })
+
+// OPEN EDIT PAGE 
+app.get("/listings/:id/edit",async (req,res)=>{
+    const{id}=req.params;
+    const editPost=await Listing.findById(id);
+    res.render("./listings/edit.ejs",{editPost})
+})
+
+// route to add new object to database 
+app.post("/listings",async(req,res)=>{
+    const newListings=new Listing(req.body.listings);
+    await newListings.save();
+    res.redirect("/listings")
+})
+
+// ROUTE TO UPDATE OBJECT 
+app.put("/listings/:id",async(req,res)=>{
+    const {id}=req.params;
+    // const updatedPost=await req.body.listings;
+   await Listing.findByIdAndUpdate(id,{...req.body.listings})
+   res.redirect(`/listings/${id}`);
+})
+
+// DELETE ROUTE 
+app.delete("/listings/:id",async (req,res)=>{
+    const {id}=req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings");
+})
+
 app.listen(3000, () => {
     console.log("Server listining at port 3000");
 })
