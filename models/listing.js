675055385d4147fe97266f20aa59be1878064ec8
Ayush Joshi <ayushjoshi3699@gmail.com>
2025-01-16 15:50:40 +0530

    inhance structure

--- models/listing.js
+++ models/listing.js
@@ -1,39 +1,43 @@
 const mongoose = require("mongoose");
-const Schema=require('mongoose');
+const Schema = require("mongoose");
 
-const listingSchema =new mongoose.Schema({
+const listingSchema = new mongoose.Schema({
   title: {
     type: String,
     require: true,
-    unique:true,
+    unique: true,
   },
   description: {
     type: String,
   },
-  url:{
-    type:String,
-    default:"https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    set:(v)=> v===""?"https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D":v,
+  url: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
   },
-  price:{
-    type:Number,
-    require:true,
+  price: {
+    type: Number,
+    require: true,
   },
-  location:{
-    type:String,
-    require:true
+  location: {
+    type: String,
+    require: true,
   },
-  country:{
-    type:String,
-    require:true,
+  country: {
+    type: String,
+    require: true,
   },
-  reviews:[
+  reviews: [
     {
-      type:Schema.Types.ObjectId,
-      ref:"Review",
+      type: Schema.Types.ObjectId,
+      ref: "Review",
     },
-  ]
+  ],
 });
 
-const Listing=mongoose.model("Listing", listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
